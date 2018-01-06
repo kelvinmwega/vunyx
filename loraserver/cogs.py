2af5a53e1baa92d995bfa373c34391c43afe782f
vunyx <kmwega@gmail.com>
2018-01-06 21:49:24 +0300

    Add files via upload

---
+++ loraserver/cogs.py
@@ -0,0 +1,76 @@
+import requests
+import os
+import urllib2, base64
+import datetime
+import csv
+from dateutil import parser
+from dateutil import tz
+
+
+cloudantHost = "https://8d7e1f8e-1f7d-4d64-8788-490379bcbad5-bluemix.cloudant.com/"
+cloudantusername = "8d7e1f8e-1f7d-4d64-8788-490379bcbad5-bluemix"
+cloudantPassword = "b69d78f8c5aad962b42e278c73cce8238522bdd4a73aaeec73fa8efc11524db3"
+operationsDB = "operations/_find"
+
+selector = {}
+timestamp = {}
+reqObj = {}
+sortObj = {}
+sortArray = []
+sort = {}
+
+class reqHandler(object):
+    """docstring for reqHandler."""
+    def __init__(self, dev):
+        super(reqHandler, self).__init__()
+        self.dev = dev
+
+    def getDev(dev):
+        try:
+
+            #get system time in UTC
+            currentTime = datetime.datetime.utcnow()
+            #Encode Authorization string using password and username
+            base64string = base64.encodestring('%s:%s' % (cloudantusername, cloudantPassword)).replace('\n', '')
+
+            #create headers json object
+            headers = {'content-type' : 'application/json', "Authorization" : "Basic %s" % base64string}
+            headersObj = json.dumps(headers)
+
+            #convert time to ISO string and add to request body object
+            timestamp["$lt"] = currentTime.strftime('%Y-%m-%dT%H:%M:%S.000Z')
+
+            selector["timestamp"] = timestamp
+            selector["deviceId"] = dev
+
+            sortObj["timestamp"] = "desc"
+            sortArray.append(sortObj)
+
+            #Final request body object
+            reqObj["selector"] = selector
+            reqObj["sort"] = sortArray
+            # reqObj["limit"] = int(dataLimit)
+
+            print "Request Body Object..."
+            print reqObj
+
+            #Post the requst body
+            resp = requests.post(data = json.dumps(reqObj), url = cloudantHost+levelmeterDB, headers = headers)
+
+            print "Response status code.. " + str(resp.status_code)
+
+            #check whether request was succesful.
+            if (resp.status_code == 200):
+
+                #parse response string into a json object
+                respObj = json.loads(resp.text)
+                return respObj
+
+            else:
+                print "Encountered error fetching data, check internet connection"
+                return resp.status_code
+
+
+
+        except Exception as e:
+            return "err"
---
+++ loraserver/loraserver.py
@@ -0,0 +1,290 @@
+#
+# Purpose:
+#     Lora Network server. Implements UDP server that listens for udp packets from semtech packet forwarder application
+#     received data is sent to IBM watson IoT via mqtt, the code is configured as a gateway.
+#
+# Author :
+#     Kelvin Mwega - kelvinmk@ke.ibm.com
+#
+# Hardware :
+#     Built By : Reha Yurdakul - rehay@ke.ibm.com
+#     Raspberry Pi 3.
+#     IMST IC880A Concentrator.
+#
+# Version 1 : 15.May.2017 by kelvinmk@ke.ibm.com
+#     - UDP server implemented and tested
+#
+# Version 2 : 16.May.2017 by kelvinmk@ke.ibm.com
+#     - Configured as a gateway to watson IoT.
+#     - received UDP data sent to watdson IoT via MQTTself.
+#
+# Version 3 : 27.May.2017 by kelvinmk@ke.ibm.com
+#     - Modified to send back acknowledgement packet to packet forwarder applicationself.
+
+
+import socket
+import sys
+import binascii
+import ibmiotf.gateway
+import json
+import array
+import time
+import os,binascii
+from flask import Flask, jsonify
+from cogs import getDev
+
+# Organization ID 14br69
+# Device Type LoRaGW
+# Device ID   LoRaGW01
+# Authentication Method   token
+# Authentication Token    vunyxloragw
+
+dataEncoded = None
+########## Watson IoT MQTT Functions ###########
+
+app = Flask(__name__)
+
+@app.route('/')
+def Welcome():
+    return app.send_static_file('index.html')
+
+@app.route('/myapp')
+def WelcomeToMyapp():
+    return 'Welcome again to my app running on Bluemix!'
+
+
+
+def myOnPublishCallback():
+       print("Confirmed event received by IBM Watson IoT Platform\n")
+       print >>sys.stderr, '##############################################'
+       print >>sys.stderr, '\nwaiting to receive message'
+
+
+########## Utilities ###########
+
+def int_to_bytes(value, length):
+    result = []
+
+    for i in range(0, length):
+        result.append(value >> (i * 8) & 0xff)
+
+    result.reverse()
+
+    return result
+
+def HexToByte( hexStr ):
+    bytes = []
+
+    hexStr = ''.join( hexStr.split(" ") )
+
+    for i in range(0, len(hexStr), 2):
+        bytes.append( chr( int (hexStr[i:i+2], 16 ) ) )
+
+    return ''.join( bytes )
+
+def pktSentConfirm(msgTx):
+    #msgRx
+    if (checkNodeAddress(msgTx) == True):
+        print "Not my message " + msgTx.decode('base64').split(',')[0]  + "  " + respConstructor(msgTx).split(',')[0]
+        return True
+
+    print "My message " + msgTx.decode('base64')
+    return False
+
+def respConstructor(msgRx):
+
+    try:
+        strToReturn = "GW01," + msgRx.decode('base64').split(',')[0]
+    except:
+        strToReturn = "GW01,EP002"
+
+    return strToReturn
+
+def checkNodeAddress(nodeId):
+    print "########## " + nodeId.decode('base64').split(',')[0]
+    nodesArray = ["EP001", "EP002", "EP003", "TestNode5", "GPSNode"]
+    print(getDev(nodeId))
+    if nodeId.decode('base64').split(',')[0] in nodesArray:
+        dataString = ("GW01," + str(nodeId.decode('base64').split(',')[0])).encode('base64')
+        global dataEncoded
+        dataEncoded = dataString
+        print dataString
+        return True
+
+    return False
+
+def postToWatsonIoT(pktTProcess):
+
+    objToSend = json.loads(pktTProcess)
+    nodeIdentifier = json.loads(pktTProcess)['rxpk'][0]['data'].decode('base64').split(',')[0]
+    nodeData01 = json.loads(pktTProcess)['rxpk'][0]['data'].decode('base64').split(',')[1]
+
+    objToSend['rxpk'][0]['nodeID'] = nodeIdentifier
+    objToSend['rxpk'][0]['nodeData01'] = nodeData01
+
+    print (objToSend)
+
+    gatewaySuccess = gatewayCli.publishGatewayEvent("event", "json", objToSend, qos=1, on_publish=myOnPublishCallback )
+    deviceSuccess = gatewayCli.publishDeviceEvent("Everporimeter", nodeIdentifier, "event", "json", objToSend, qos=1, on_publish=myOnPublishCallback )
+
+    if not gatewaySuccess:
+        print("Gateway not connected to IBM Watson IoT Platform while publishing from Gateway")
+
+    if not deviceSuccess:
+        print("Gateway not connected to IBM Watson IoT Platform while publishing from Gateway on behalf of a device")
+
+
+########## Handlers ###########
+
+def respondPushAck():
+    dataAck = bytearray()
+
+
+
+port = os.getenv('PORT', '5000')
+
+if __name__ == '__main__':
+    #>>> binascii.hexlify(bytearray(array_alpha))
+    # Create a TCP/IP socket
+
+    UDP_IP_ADDRESS = "localhost"
+    UDP_PORT_NO = 1680
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    organization = "14br69"
+    gatewayType = "LoRaGW"
+    gatewayId = "LoRaGW01"
+
+    authMethod = "token"
+    authToken = "vunyxloragw"
+    gatewayOptions = {"org": organization, "type": gatewayType, "id": gatewayId, "auth-method": authMethod, "auth-token": authToken}
+    gatewayCli = ibmiotf.gateway.Client(gatewayOptions)
+
+    gatewayCli.connect()
+
+    # Bind the socket to the port
+    server_address = ('localhost', 1690)
+    print >>sys.stderr, 'starting up on %s port %s' % server_address
+    sock.bind(server_address)
+
+    app.run(host='0.0.0.0', port=int(port))
+
+    pullAddress = None
+    pullTocken = None
+    pullGatewayId = None
+
+    #sent = sock.sendto("test", (UDP_IP_ADDRESS, UDP_PORT_NO))
+
+    while True:
+        try :
+            dataAck = bytearray()
+            dataResp = bytearray()
+            data, address = sock.recvfrom(4096)
+            data = bytearray(buffer(data))
+
+            protVersion = data[0]
+            token = data[1:3]
+            identifier = data[3]
+            gwID = data[4:12]
+            rawData = data[12:]
+
+            print str(identifier)
+
+            if str(identifier) == "0":
+                try:
+
+                    txPkt = {}
+                    pktToRsp = {}
+                    dataToSend = rawData.decode("utf-8")
+                    #gatewayCli.setMessageEncoderModule('json', jsonCodec)
+                    rxPkt = json.loads(dataToSend)
+                    rxPktArray = rxPkt['rxpk'][0]
+
+                    dataAck.extend(int_to_bytes(protVersion, 1))
+                    dataAck.extend(token)
+                    dataAck.extend(int_to_bytes(1, 1))
+
+                    if dataAck:
+                        #sent = sock.sendto(data, address)
+                        sent = sock.sendto(buffer(dataAck), address)
+                        print >>sys.stderr, 'sent PUSH_ACK %s bytes back to %s' % (sent, address)
+
+                    if (pktSentConfirm(rxPktArray['data']) == True):
+
+                        postToWatsonIoT(dataToSend)
+
+                        pktToRsp['imme'] = True
+                        pktToRsp['freq'] = rxPktArray['freq']
+                        pktToRsp['rfch'] = 0
+                        pktToRsp['powe'] = 14
+                        pktToRsp['modu'] = 'LORA'
+                        pktToRsp['datr'] = rxPktArray['datr']
+                        pktToRsp['codr'] = rxPktArray['codr']
+                        pktToRsp['ipol'] = False
+                        #pktToRsp['prea'] = 8
+                        pktToRsp['size'] = 10
+                        pktToRsp["data"] = dataEncoded.strip()
+                        txPkt['txpk'] = pktToRsp
+
+                        #print(txPkt)
+                        #print "#########  " + rxPktArray['data'] + " --- " + pktToRsp['data'] + " --- " + pktToRsp['data'].decode('base64')
+                        time.sleep(2)
+
+                        try: #Check for PULL_ACK
+
+                            pullTocken = HexToByte(binascii.b2a_hex(os.urandom(2)))
+                            dataResp.extend(int_to_bytes(protVersion, 1))
+                            dataResp.extend(pullTocken)
+                            dataResp.extend(int_to_bytes(3, 1))
+                            dataResp.extend(json.dumps(txPkt))
+
+                            #print dataResp[0]
+                            #print dataResp[1:3]
+                            #print dataResp[3]
+                            #print dataResp[4:]
+                            #print json.dumps(pktToRsp)
+
+                            sent = sock.sendto(buffer(dataResp), pullAddress)
+                            print >>sys.stderr, 'sent PULL_RESP %s bytes back to %s' % (sent, pullAddress)
+
+                        except Exception as e:
+                            pass
+
+                except Exception as e:
+
+                    rxPktArray = rxPkt['stat']
+                    print rxPktArray
+
+            elif str(identifier) == "2":
+                dataAck.extend(int_to_bytes(protVersion, 1))
+                dataAck.extend(token)
+                dataAck.extend(int_to_bytes(4, 1))
+
+                #print(buffer(dataAck))
+
+                if dataAck:
+                    #sent = sock.sendto(data, address)
+                    sent = sock.sendto(buffer(dataAck), address)
+                    print >>sys.stderr, 'sent PULL_ACK %s bytes back to %s' % (sent, address)
+                    pullAddress = address
+                    pullTocken = token
+                    pullGatewayId = gwID
+
+            elif str(identifier) == "5":
+
+                dataAck.extend(int_to_bytes(protVersion, 1))
+                dataAck.extend(token)
+                dataAck.extend(int_to_bytes(4, 1))
+
+                #print >>sys.stderr, '##### received %s bytes from %s' % (len(data), address)
+                print >>sys.stderr, ("Protocol Version -- " + str(protVersion))
+                print >>sys.stderr, ("Token -- " + str(binascii.hexlify(token)))
+                print >>sys.stderr, ("Identifier -- ") + str(identifier)
+                # print >>sys.stderr, ("Gateway ID -- " + str(binascii.hexlify(gwID)))
+                # print >>sys.stderr, ("LoRa Payload --" + rawData )
+
+        except KeyboardInterrupt :
+            break
+
+    print("\nLogging off....")
